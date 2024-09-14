79c3358419a550f37cc55ac7b382bf3b379a5943
smohyud4 <smohyuddin4@outlook.com>
2024-09-14 16:19:15 -0500

    added analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Analytics />
     <App />
   </BrowserRouter>
 );
