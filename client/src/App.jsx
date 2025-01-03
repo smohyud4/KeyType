ec3d9c00d1cdb9615499c8853bf677ae233ffb91
smohyud4 <smohyuddin4@outlook.com>
2025-01-03 02:32:22 -0600

    removed unecessary routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
 /* eslint-disable no-unused-vars */
 import {Route, Routes} from 'react-router-dom';
 //import NavBar from './components/NavBar'
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Account from './pages/Account';
 import Race from './pages/Race';
 import Practice from './pages/Practice';
 import Footer from './components/Footer/Footer';
@@ -15,10 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/account' element={<Account/>}/>
+        <Route path='/' element={<Race/>}/>
         <Route path='/race' element={<Race/>}/>
         <Route path='/practice' element={<Practice/>}></Route>
       </Routes>
